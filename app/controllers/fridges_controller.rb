5299446c880508ad0dbfa421b659500664e8de40
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-02-06 22:32:20 -0500

    index, new, create all working

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -2,5 +2,30 @@ class FridgesController < ApplicationController
     def index
         @fridges = Fridge.all
     end
+
+    def new
+        @fridge = Fridge.new
+    end
+
+    def create
+        @fridge = Fridge.new(fridge_params)
+        if @fridge.save
+            redirect_to fridges_url
+        else
+            reidrect_to fridges_new_url
+        end
+
+    end
+    
+    # def show
+    #     @fridge = Fridge.find_by(name: 
+    # end
+    
+    private
+    def fridge_params
+        params.require(:fridge).permit(:location, :brand, :size)
+    end
+
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/fridges', to: 'fridges#index'
   get '/fridges/new', to: 'fridges#new'
   post '/fridges', to: 'fridges#create'
+  get '/fridges/:id', to: 'fridges#show'
 end
